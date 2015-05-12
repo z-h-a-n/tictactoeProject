5b59f90861aa338b1322aeff83b0546cb14bf0d6
zhan wang <zhanzw@gmail.com>
2015-05-12 10:39:32 +0100

    dispay first player in the play grid

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -7,22 +7,41 @@ $(document).ready(function(){
 	function choosePlay(){
 		if ($(this).attr("id") === "playerX") {
 			var player1 = "x";
-			// setP1(player1);
+			console.log("1st player has chosen x");
 		} else {
 			var player1 = "o";
-			// setP1(player1);
+			console.log("1st player has chosen o");
 		}
+
+
 
 		$(".playInput").on("click", set1stInput);
 		var playInputsArray = [];
 		function set1stInput(){
-			if ($(this).attr("id") === "0") {
-				var totalInputs = playInputsArray.push(player1);
-				console.log(playInputsArray);
-			} else if ($(this).attr("id") === "1") {
-				var totalInputs = playInputsArray.push(player1);
-				console.log(playInputsArray);
-			}
+			if (playInputsArray.length === 0) {
+				if ($(this).attr("id") === "0") {
+					var totalInputs = playInputsArray.push(player1);
+					$(this).text(player1)
+					console.log(playInputsArray);
+				} else if ($(this).attr("id") === "1") {
+					var totalInputs = playInputsArray.push(player1);
+					$(this).text(player1);
+					console.log(playInputsArray);
+					//add if statement for other keys
+				}
+			} else if (playInputsArray.length > 0) {
+					//allow input as long as no winner and array.lengh < 9
+						if (playInputsArray[playInputsArray.length - 1] === "x" && playInputsArray.length < 9 ) {
+							var totalInputs = playInputsArray.push("o");
+							console.log(playInputsArray);
+						} else if (playInputsArray[playInputsArray.length - 1] === "o" && playInputsArray.length < 9) {
+							var totalInputs = playInputsArray.push("x");
+							console.log(playInputsArray);
+
+						}
+					
+			}// end of else if playInputsArray.length > 0
+
 		}	
 
 
