468c52048911ad29ef4b0e1a35eda895503c128b
zhan wang <zhanzw@gmail.com>
2015-05-12 16:38:15 +0100

    working / added count clicks to show correctly when it's a tie and added condition for switch to prevent undefined winning

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -19,6 +19,8 @@ $(document).ready(function(){
 
 		var playInputsArray = [undefined, undefined, undefined, undefined,undefined, undefined, undefined, undefined, undefined];
 		var lastInput = "";
+		var buttonClicks = "";
+
 
 		function setInput(){
 		  //if input array is empty
@@ -80,47 +82,54 @@ $(document).ready(function(){
 						lastInput = "x";
 			};//if
 
-		winner(playInputsArray);
+			buttonClicks = Number(buttonClicks) + 1;
+			
+
+		winner(playInputsArray, buttonClicks);
 		};//function setInput	
 
 
-		function winner(playInputsArray) {
-		var winnerArray = playInputsArray;
+		//check all 8 conditions to determin if x or o wins or tie
+		//max button input is 9 [need to track how many times a button is clicked]
+		//if buttonClicks < 9 && there is a winner => display winner
+		//if buttonClicks = 9 => display tie		
+		function winner(playInputsArray, buttonClicks) {
+			var winnerArray = playInputsArray;
+			var numClicks = buttonClicks;
 
-		for (i=0; i<9; i++) {
-			var winCombi = true ;
-			switch (winCombi) {
-				case winnerArray[0] === winnerArray[1] && winnerArray[1] === winnerArray[2]:
-					$(".winner").text(winnerArray[1] + " won");
-				break;
-				case winnerArray[3] === winnerArray[4] && winnerArray[4] === winnerArray[5]:
-					$(".winner").text(winnerArray[4] + " won");
-				break;
-				case winnerArray[6] === winnerArray[7] && winnerArray[7] === winnerArray[8]:
-					$(".winner").text(winnerArray[7] + " won");
-				break;
-				case winnerArray[0] === winnerArray[3] && winnerArray[3] === winnerArray[6]:
-					$(".winner").text(winnerArray[3] + " won");
-				break;
-				case winnerArray[1] === winnerArray[4] && winnerArray[4] === winnerArray[7]:
-					$(".winner").text(winnerArray[4] + " won");
-				break;
-				case winnerArray[2] === winnerArray[5] && winnerArray[5] === winnerArray[8]:
-					$(".winner").text(winnerArray[5] + " won");
-				break;
-				case winnerArray[0] === winnerArray[4] && winnerArray[4] === winnerArray[8]:
-					$(".winner").text(winnerArray[4] + " won");
-				break;
-				case winnerArray[2] === winnerArray[4] && winnerArray[4] === winnerArray[6]:
-					$(".winner").text(winnerArray[4] + " won");
-				break;
-				default: 
-					$(".winner").text("It's a tie");
+			if (numClicks <= 9) {
+				var winCombi = true;
+				switch (winCombi) {
+					case winnerArray[0] === winnerArray[1] && winnerArray[1] === winnerArray[2] && winnerArray[1] !== undefined:
+						$(".winner").text(winnerArray[1] + " won");
+					break;
+					case winnerArray[3] === winnerArray[4] && winnerArray[4] === winnerArray[5] && winnerArray[4] !== undefined:
+						$(".winner").text(winnerArray[4] + " won");
+					break;
+					case winnerArray[6] === winnerArray[7] && winnerArray[7] === winnerArray[8] && winnerArray[7] !== undefined:
+						$(".winner").text(winnerArray[7] + " won");
+					break;
+					case winnerArray[0] === winnerArray[3] && winnerArray[3] === winnerArray[6] && winnerArray[3] !== undefined:
+						$(".winner").text(winnerArray[3] + " won");
+					break;
+					case winnerArray[1] === winnerArray[4] && winnerArray[4] === winnerArray[7] && winnerArray[4] !== undefined:
+						$(".winner").text(winnerArray[4] + " won");
+					break;
+					case winnerArray[2] === winnerArray[5] && winnerArray[5] === winnerArray[8] && winnerArray[5] !== undefined:
+						$(".winner").text(winnerArray[5] + " won");
+					break;
+					case winnerArray[0] === winnerArray[4] && winnerArray[4] === winnerArray[8] && winnerArray[4] !== undefined:
+						$(".winner").text(winnerArray[4] + " won");
+					break;
+					case winnerArray[2] === winnerArray[4] && winnerArray[4] === winnerArray[6] && winnerArray[4] !== undefined:
+						$(".winner").text(winnerArray[4] + " won");
+					break;
+					case numClicks >= 9: 
+						$(".winner").text("It's a tie");
+				};
 			};
-		};
 
 		console.log(winnerArray);
-		//check all 8 conditions to determin if x or o wins or tie
 
 		};
 
